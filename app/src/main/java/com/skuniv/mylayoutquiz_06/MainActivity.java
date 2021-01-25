652ed283b89b9a39dc82727dc2e24173fa1b0f34
JaeHyuen-Jung <jgjg7882@naver.com>
2021-01-25 19:20:17 +0900

    new Project

---
+++ app/src/main/java/com/skuniv/mylayoutquiz_06/MainActivity.java
@@ -0,0 +1,17 @@
+package com.skuniv.mylayoutquiz_06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
