7b99c817bf93ec63787c9fd3a7d18547799cba7a
JaeHyuen-Jung <jgjg7882@naver.com>
2021-01-25 20:22:35 +0900

    new Project

--- app/src/main/java/com/skuniv/mylayoutquiz_06/MainActivity.java
+++ app/src/main/java/com/skuniv/mylayoutquiz_06/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button= findViewById(R.id.btn_01);
+//        fdsfsfsdfsdfsdfsdfsdfsd
 
     }
 }
