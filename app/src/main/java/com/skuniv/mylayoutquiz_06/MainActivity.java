33b0953cc8d144397cf116cc4c9dbdf9f0deb54a
JaeHyuen-Jung <jgjg7882@naver.com>
2021-01-26 00:04:20 +0900

    form 실행

--- app/src/main/java/com/skuniv/mylayoutquiz_06/MainActivity.java
+++ app/src/main/java/com/skuniv/mylayoutquiz_06/MainActivity.java
@@ -16,12 +16,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button button= findViewById(R.id.btn_01);
 //        fdsfsfsdfsdfsdfsdfsdfsd
-        //tretretertertertretertert
-//        esfsrwerwe
-        //ㄴㅇ라아아앙 akakakakakakakaakak313131313214192375692384756asowekudfghalskedfjhbnalksdjhflqikweuyjjj
-    //update12343334757575757575757575757575
-        //sadfasdfasd
-        //동동동대문을 열어라 남남남대문을 열어라 아ㅣ쉬워벌써 12시~ 어떻게 벌써 12시네
-        //
     }
 }
