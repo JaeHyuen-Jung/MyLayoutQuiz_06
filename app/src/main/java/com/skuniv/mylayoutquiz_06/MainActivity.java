878f50cb36982c39d6246d41f6fac27b492c446e
Novice9999 <rlgur1019@naver.com>
2021-01-25 21:46:46 +0900

    까꿍

--- app/src/main/java/com/skuniv/mylayoutquiz_06/MainActivity.java
+++ app/src/main/java/com/skuniv/mylayoutquiz_06/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         //ㄴㅇ라아아앙 akakakakakakakaakak313131313214192375692384756asowekudfghalskedfjhbnalksdjhflqikweuyjjj
     //update12343334757575757575757575757575
         //sadfasdfasd
-        //동동동대문을 열어라 남남남대문을 열어라 아ㅣ쉬워벌써 12시~
+        //동동동대문을 열어라 남남남대문을 열어라 아ㅣ쉬워벌써 12시~ 어떻게 벌써 12시네
+        //
     }
 }
